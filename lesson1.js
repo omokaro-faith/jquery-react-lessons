1f6a0bd29896b95a45fb471472068386aaef13e4
andela-fomokaro <faith.omokaro@andela.com>
2019-01-13 18:59:10 +0100

    first lesson

---
+++ lesson1.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  constructor(){
+    super();
+    this.state = {
+      toggle: false
+    }
+  }
+  testClick(e) {
+    e.preventDefault();
+    let isToggled = this.state.toggle;
+    this.setState({
+      toggle: !isToggled
+    });
+  }
+
+  render(){
+    let buttonClass = this.state.toggle ? 'toggled' : '';
+    return(
+      <div id="wrapper" className={buttonClass}>
+        <button id="menu-toggle" onClick={this.testClick}>Test onClick</button>
+      </div>
+    )
+  }
+}
